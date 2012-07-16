f22a2503e66149a91d02b6b06bf386298de0830d
Will Furnass <will@thearete.co.uk>
2012-07-16 10:14:24 +0100

    Updated setup.py to use distribute/setuptools rather than distutils

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
+import os 
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='PyShoal',
-    version='0.1.0',
-    author='Will Furnass',
-    author_email='will@thearete.co.uk',
-    packages=['pyshoal', 'pyshoal.test'],
-    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    #url='http://pypi.python.org/pypi/PSO/',
-    license='LICENSE.txt',
-    description='Particle Swarm Optimisation implementation.',
-    long_description=open('README.txt').read(),
-    install_requires=[
+    name = 'PyShoal', # Y
+    version = '0.1.1', # Y
+    #packages=['pyshoal', 'pyshoal.test'],
+    packages = find_packages(),
+
+    install_requires = [
         "numpy >= 1.5.1",
         "matplotlib >= 1.0.1",
     ],
+
+    author = 'Will Furnass',
+    author_email = 'will@thearete.co.uk',
+    description='Particle Swarm Optimisation implementation.',
+    license='GPL 3.0',
+    keywords='particle swarm optimisation optimization'
+    url='http://pypi.python.org/pypi/PyShoal/',
+    long_description=read('README'),
 )
